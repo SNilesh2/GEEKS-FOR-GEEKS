91345d42fbd400e16565bfecfac0ab195a327682
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2025-01-17 10:57:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] arr = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) arr[idx++] = i;
+
+            new Solution().mergeSort(arr, 0, arr.length - 1);
+
+            for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    void mergeSort(int arr[], int l, int r) {
+        // code here
+        if(l>=r) return;
+        int m=l+(r-l)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+    public void merge(int[] arr,int low,int mid,int high)
+    {
+        int left=low;
+        int right=mid+1;
+        ArrayList<Integer> temp=new ArrayList<>();
+        while(left<=mid && right<=high)
+        {
+            if(arr[left]<=arr[right])
+            {
+                temp.add(arr[left]);
+                left++;
+            }
+            else
+            {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while(left<=mid)
+        {
+            temp.add(arr[left]);
+            left++;
+        }
+        while(right<=high)
+        {
+            temp.add(arr[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++)
+        {
+            arr[i]=temp.get(i-low);
+        }
+    }
+}
