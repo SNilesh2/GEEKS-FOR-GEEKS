38f9f5b04990a42510dc6ce3dc90b711aa13c6da
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2025-06-19 15:00:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,82 +1,76 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            boolean ans = obj.isCycle(V, adj);
-            if (ans)
-                System.out.println("1");
-            else
-                System.out.println("0");
-        }
-    }
-}
-// } Driver Code Ends
-
 class Pair
 {
-    int first;
-    int second;
-    Pair(int _first,int _second)
+    int current;
+    int parent;
+    public Pair(int current,int parent)
     {
-        this.first=_first;
-        this.second=_second;
+        this.current=current;
+        this.parent=parent;
+    }
+}
+class Solution {
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        
+        for(int i=0;i<V;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        
+        boolean[] visited = new boolean[V];
+        
+        for(int i=0;i<V;i++)
+        {
+            if(visited[i]==false)
+            {
+                if(detectcycle(i,adj,visited))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean detectcycle(int start,ArrayList<ArrayList<Integer>> adj,boolean[] visited )
+    {
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(start,-1));
+        visited[start] = true;
+        while(!q.isEmpty())
+        {
+            int current = q.peek().current; 
+            int parent = q.peek().parent;
+            q.poll();
+            for(int node : adj.get(current))
+            {
+                if(visited[node]==false)
+                {
+                    visited[node]=true;
+                    q.add(new Pair(node,current));
+                }
+                else if(parent != node)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
-class Solution {
-    // Function to detect cycle in an undirected graph.
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
-        boolean[] vis=new boolean[V];
-        Arrays.fill(vis,false);
-        for(int i=0;i<V;i++)
-        {
-            if(vis[i]==false)
-            {
-                if(iscycle(i,adj,vis)==true) return true;
-            }
-        }
-        return false;
-    }
-    public static boolean iscycle(int src,ArrayList<ArrayList<Integer>> adj,boolean[] vis)
-    {
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(src,-1));
-        vis[src]=true;
-        while(!q.isEmpty())
-        {
-            int node=q.peek().first;
-            int parent=q.peek().second;
-            q.remove();
-            for(int adjacentnode:adj.get(node))
-            {
-                if(vis[adjacentnode]==false)
-                {
-                    q.add(new Pair(adjacentnode,node));
-                    vis[adjacentnode]=true;
-                }
-                else if(parent!=adjacentnode && vis[adjacentnode]==true) return true;
-            }
-        }
-        return false;
-    }
-}
+
+
+
+
+
+
+
+
